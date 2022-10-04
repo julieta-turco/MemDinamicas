6c13a576c516bd676c4b22cd2e8782c2d93d7dc1
julieta turco <julivickiturco23@gmail.com>
2022-10-04 00:01:04 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace EstructuradeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            Queue cola = new Queue();
+            Console.WriteLine("ingrese una cadena de texto");
+            cadena = Console.ReadLine();
+
+            while (cadena != string.Empty)
+            {
+                cola.Enqueue(cadena);
+
+                Console.WriteLine("ingrese la siguiente cadena de texto, si asi lo desea");
+                Console.ReadLine();
+            }
+            while (cola.Count > 0)
+            {
+                Console.WriteLine((string)cola.Dequeue());
+            }
+           Console.ReadKey();
+            
+        }
+    }
+}
